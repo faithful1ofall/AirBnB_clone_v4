36dc0cd30f3eff9358fff0242b318ca212338aa6
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-04-29 13:37:28 +0100

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -99,8 +99,8 @@ class DBStorage:
     def get(self, cls, id):
         """Retrieve an object"""
         if cls is not None and type(cls) is str and id is not None and\
-           type(id) is str and cls in name2class:
-            cls = name2class[cls]
+           type(id) is str and cls in self.__clsdict:
+            cls = self.__clsdict[cls]
             result = self.__session.query(cls).filter(cls.id == id).first()
             return result
         else:
@@ -109,8 +109,8 @@ class DBStorage:
     def count(self, cls=None):
         """Count number of objects in storage"""
         total = 0
-        if type(cls) == str and cls in name2class:
-            cls = name2class[cls]
+        if type(cls) == str and cls in self.__clsdict:
+            cls = self.__clsdict[cls]
             total = self.__session.query(cls).count()
         elif cls is None:
             for cls in name2class.values():
